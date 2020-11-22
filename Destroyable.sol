d6b34ce8c7643dab65f6580815c584d40ff57e6a
metaverde <metatrive@gmail.com>
2020-11-22 12:33:54 -0800

    Adding notes to practice GitHub from VSCode

--- Destroyable.sol
+++ Destroyable.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.5.12;
 import "./Ownable.sol";
 
+/*adding notes to practice GitHub from VSCode*/
+
 contract Destroyable is Ownable {
     
     function close() public onlyOwner { //onlyOwner is custom modifier
